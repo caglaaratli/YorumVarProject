1b299bdd43e77f2fba95e7d7fe904987b8e2dde4
caglaaratli <caglaratlii@gmail.com>
2024-04-22 18:48:59 +0300

    add responsive

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,29 +1,29 @@
-
 import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header className="bg-white shadow-md py-4 px-6">
-      <div className="flex items-center px-16 justify-between">
+    <header className="bg-white shadow-md py-2 sm:py-4 px-4 sm:px-6">
+      <div className="flex items-center justify-between">
+        {/* Logo ve Navigasyon Linkleri */}
+        <div className="flex items-center space-x-4 flex-grow pl-4 sm:pl-16"> {/* Sol içeriye kaydırma */}
+          <img src="/logo.png" className="h-8 sm:h-10" alt="Logo" />
 
-        {/* Logo */}
-        <div className="flex items-center space-x-4">
-          {/* U-img class'ını img elementine ekledim, bu sizin logonuzun sınıfı olacak */}
-          <img src="/logo.png" className="h-10 u-img" />
+          <div className="flex sm:hidden space-x-4">
+            <Link to="/reviews" className="text-sm text-red-400 hover:text-red-700">Reviews</Link>
+          </div>
           
-          {/* Navigasyon Linkleri */}
           <div className="hidden sm:flex space-x-4">
-            <Link to="/reviews" className="text-red-400 hover:text-red-700">Değerlendirmeler</Link>
+            <Link to="/reviews" className="text-sm text-red-400 hover:text-red-700">Reviews</Link>
           </div>
         </div>
 
         {/* Sağ Üst Köşedeki Butonlar */}
-        <div className="flex items-center  space-x-4  ">
-          <Link to="/login" className="text-gray-600 hover:text-zinc-900  ">Login</Link>
-          <span className="text-gray-400">/</span>
-          <Link to="/register" className="text-gray-600 hover:text-zinc-900 ">Sign Up</Link>
-          <button className="bg-red-400 hover:bg-red-500 text-white font-semibold py-2 px-4 rounded">
-            + Yorum Yaz
+        <div className="flex items-center space-x-2 sm:space-x-4 pr-4 sm:pr-16"> {/* Sağ içeriye kaydırma */}
+          <Link to="/login" className="text-xs sm:text-sm text-gray-600 hover:text-zinc-900">Login</Link>
+          <span className="text-gray-400 text-xs sm:text-sm">/</span>
+          <Link to="/register" className="text-xs sm:text-sm text-gray-600 hover:text-zinc-900">Sign Up</Link>
+          <button className="bg-red-400 hover:bg-red-500 text-white font-semibold py-1 sm:py-2 px-2 sm:px-4 rounded text-xs sm:text-sm">
+            + Comment
           </button>
         </div>
       </div>
