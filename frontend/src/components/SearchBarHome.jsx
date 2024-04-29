6cec8cd8337989038a33d686f9633457f9959dd9
caglaaratli <caglaratlii@gmail.com>
2024-04-30 00:03:03 +0300

    search button text has been changed

--- frontend/src/components/SearchBarHome.jsx
+++ frontend/src/components/SearchBarHome.jsx
@@ -9,10 +9,10 @@ const SearchBar = () => {
         <div className="p-4"> {/* Paddingi artırdık */}
           <Icon iconSet={iconSet} icon="search" className="w-6 h-6" />
         </div>
-        <input type="text" className="px-4 py-3 w-full sm:w-64 focus:outline-none rounded-full" placeholder="Ürün ara" />
+        <input type="text" className="px-4 py-3 w-full sm:w-64 focus:outline-none rounded-full" placeholder="Search product" />
         {/* Genişlik ve yükseklik artırıldı, sm:w-48 gibi daha küçük ekranlarda uyum sağlamak için boyut sınıfları eklenebilir */}
         <button className="flex items-center justify-center bg-blue-400 hover:bg-blue-500 text-white rounded-full py-4 px-8 ml-2">
-          Ara
+          Search
         </button>
       </div>
     </div>
