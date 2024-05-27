51fd267d4ca0c8f445ae59f888469811e5445833
caglaaratli <caglaratlii@gmail.com>
2024-05-27 21:31:13 +0300

    The message is set to be displayed for 2 seconds.

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -16,29 +16,28 @@ function SignUp() {
         income.data === "Username is already in use"
       ) {
         setMessage(income.data);
+        setTimeout(() => {
+          setMessage(null);
+        }, 2000); 
       } else if (income.data === "User registered successfully") {
         setMessage(income.data);
-    
-        navigate("/login");
+        setTimeout(() => {
+          setMessage(null);
+          navigate("/login");
+        }, 2000); 
       }
     } else if (income.status === 500) {
       setMessage("Registration failed. Please try again later.");
+      setTimeout(() => {
+        setMessage(null);
+      }, 2000); 
     }
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-blue-200">
-      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-md shadow-lg">
-        <h1 className="text-2xl font-bold text-center italic text-gray-600">
-          Sign Up
-        </h1>
-        {message && (
-          <div className="text-center p-3 mb-2 bg-green-100 border border-green-400 text-green-700">
-            {message}
-          </div>
-        )}
-        <SignUpForm onSubmit={handleRegistration} setMessage={setMessage} />
-      </div>
+    <div>
+      {message && <div className="message">{message}</div>}
+      <SignUpForm handleRegistration={handleRegistration} />
     </div>
   );
 }
